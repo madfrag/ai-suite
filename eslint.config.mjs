99e4a30051b52cc620162bed73873ef1b3cb7dce
rushanengalychev <dev@engalychev.com>
2026-08-26 18:30:54 +0200

    add typescript 7 for type-check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import prettierConfig from 'eslint-plugin-prettier/recommended';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, prettierConfig, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}];
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettierConfig,
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  }
+];
 
 export default eslintConfig;
